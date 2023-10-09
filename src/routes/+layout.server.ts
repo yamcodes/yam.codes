abcd19c913ad11eaf5f7a9c1442db4730749d199
Yam Borodetsky <yamyam263@gmail.com>
2023-10-09 17:02:59 +0000

    chore: continue migration

--- src/routes/+layout.server.ts
+++
@@ -1,4 +0,0 @@
-import { env } from '$env/dynamic/private';
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = () => ({ analyticsId: env.VERCEL_ANALYTICS_ID });
