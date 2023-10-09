64c18f9df41d6ba1d379807cd5bf442ca5287a2c
Yam Borodetsky <yamyam263@gmail.com>
2023-10-09 19:33:35 +0000

    fix: lint-staged

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,6 @@ import remarkToc from 'remark-toc';
 import remarkCollapse from 'remark-collapse';
 import sitemap from '@astrojs/sitemap';
 import { SITE } from './src/config';
-
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
