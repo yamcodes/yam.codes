b14e2cb9eccc5dae84894d2cc232a17b32493bb4
Yam Borodetsky <yamyam263@gmail.com>
2023-10-12 14:45:01 +0300

    Revert "fix: images not displaying"

--- astro.config.ts
+++ astro.config.ts
@@ -1,32 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import remarkToc from 'remark-toc';
 import remarkCollapse from 'remark-collapse';
 import sitemap from '@astrojs/sitemap';
 import { SITE } from './src/config';
-import vercel from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: squooshImageService()
+  },
   site: SITE.website,
-  integrations: [
-    tailwind({
-      applyBaseStyles: false
-    }),
-    react(),
-    sitemap()
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), sitemap()],
   markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: 'Table of contents'
-        }
-      ]
-    ],
+    remarkPlugins: [remarkToc, [remarkCollapse, {
+      test: 'Table of contents'
+    }]],
     shikiConfig: {
       theme: 'one-dark-pro',
       wrap: true
@@ -38,9 +31,9 @@ export default defineConfig({
     }
   },
   scopedStyleStrategy: 'where',
-  output: 'static',
+  output: "static",
   adapter: vercel({
     imageService: true,
-    devImageService: 'sharp'
+    devImageService: 'squoosh'
   })
 });
