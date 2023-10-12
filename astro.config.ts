42a4991285d596a3d854f77ce67cef1913877148
Yam Borodetsky <yamyam263@gmail.com>
2023-10-12 11:41:37 +0000

    refactor: fix formatting

--- astro.config.ts
+++ astro.config.ts
@@ -5,18 +5,28 @@ import remarkToc from 'remark-toc';
 import remarkCollapse from 'remark-collapse';
 import sitemap from '@astrojs/sitemap';
 import { SITE } from './src/config';
-import vercel from "@astrojs/vercel/static";
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), sitemap()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    react(),
+    sitemap()
+  ],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, {
-      test: 'Table of contents'
-    }]],
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: 'Table of contents'
+        }
+      ]
+    ],
     shikiConfig: {
       theme: 'one-dark-pro',
       wrap: true
@@ -28,7 +38,7 @@ export default defineConfig({
     }
   },
   scopedStyleStrategy: 'where',
-  output: "static",
+  output: 'static',
   adapter: vercel({
     imageService: true,
     devImageService: 'sharp'
