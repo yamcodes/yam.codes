c3fe684e0f2bc7688477bfe4b4df8f1922fce7d9
yamyam263 <yamyam263@gmail.com>
2023-10-13 18:34:57 +0000

    feat: rename astropaper to yamcodes

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   website: 'https://astro-paper.pages.dev/', // replace this with your deployed domain
   author: 'Sat Naing',
   desc: 'A minimal, responsive and SEO-friendly Astro blog theme.',
-  title: 'AstroPaper',
+  title: 'yam.codes',
   ogImage: 'astropaper-og.jpg',
   lightAndDarkMode: true,
   postPerPage: 3
