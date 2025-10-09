88e0fdd20ff95cdf410a2605c7a80313c716a9e2
Yam Borodetsky <yam@yam.codes>
2025-10-09 23:18:13 +0500

    feat: add environment to Sentry config

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -28,6 +28,8 @@ Sentry.init({
 	dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 	enabled,
 
+	environment: ENV,
+
 	// Adds request headers and IP for users, for more info visit:
 	// https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
 	sendDefaultPii: true,
